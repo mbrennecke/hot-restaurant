350ef0bbcee756f232aa3751c5d90f2ffab6b4e5
MBrennecke <MBrennecke10@gmail.com>
2018-09-06 13:14:32 -0600

    getting there

---
+++ server.js
@@ -0,0 +1,56 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+var tables = [];
+
+var waitList = {};
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+// Displays all tables
+app.get("/api/tables", function(req, res) {
+  return res.json(characters);
+});
+
+// Displays waitlist
+app.get("/api/waitlist", function(req, res) {
+  return res.json(characters);
+});
+
+// Reserve Table - takes in JSON input
+app.post("/api/characters", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newcharacter = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newcharacter);
+
+  characters.push(newcharacter);
+
+  res.json(newcharacter);
+});
+
+// Starts the server to begin listening
+// ========================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
